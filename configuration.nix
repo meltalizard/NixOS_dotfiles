618bd71f766a110c47047b39d702de02e4e70b6e
meltalizard <school.mikotancinco@gmail.com>
2025-12-14 23:04:12 +0800

    NixOS Configs Waybar Unused

---
+++ configuration.nix
@@ -0,0 +1,176 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, lib, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Asia/Manila";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_PH.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "fil_PH";
+    LC_IDENTIFICATION = "fil_PH";
+    LC_MEASUREMENT = "fil_PH";
+    LC_MONETARY = "fil_PH";
+    LC_NAME = "fil_PH";
+    LC_NUMERIC = "fil_PH";
+    LC_PAPER = "fil_PH";
+    LC_TELEPHONE = "fil_PH";
+    LC_TIME = "fil_PH";
+  };
+
+  # Enable the X11 windowing system.
+  # services.xserver.enable = true;
+
+  # Enable the XFCE Desktop Environment.
+  # services.xserver.displayManager.lightdm.enable = true;
+  # services.xserver.desktopManager.xfce.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.meltalizard = {
+    isNormalUser = true;
+    description = "meltalizard";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+    #  thunderbird
+    ];
+  };
+
+  # Install firefox.
+  programs.firefox.enable = true;
+  
+  # Install Hyprland
+  programs.hyprland = {
+	enable = true;
+	xwayland.enable = true;
+ };
+  # greetd
+ services.greetd = {
+  enable = true;
+  settings = {
+    default_session = {
+    command = "${lib.getExe pkgs.greetd.tuigreet} --time --remember --cmd Hyprland";
+    user = "greeter";
+    };
+  };
+};
+  #kitty
+  #programs.kitty = {
+  #enable = true;
+  #};
+
+  #seatd 
+  services.seatd.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+  waybar
+  wofi
+  hyprpaper
+  vim
+  kitty
+  wl-clipboard
+  neovim
+  kdePackages.dolphin
+  obsidian
+  vscode
+
+  #For Development
+  pkgs.git gcc clang
+  gdb lldb
+  cmake
+  flutter dart
+  python315
+  
+
+
+  brave
+  zip
+  libnotify
+  discord
+
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.11"; # Did you read the comment?
+
+}
