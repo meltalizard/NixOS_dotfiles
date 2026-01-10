54d19a1dd951bcd51084c2b1420e238a224b953d
meltalizard <school.mikotancinco@gmail.com>
2026-01-10 22:52:09 +0800

    messy config (replace and archive everything

--- configuration.nix
+++ configuration.nix
@@ -1,42 +1,28 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
-{ config, pkgs, lib, ... }:
-
+# and in the NixOS manual (accessible by running 'nixos-help').
+{ config, pkgs, ... }:
 {
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
     ];
+    nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
   # Enable networking
   networking.networkmanager.enable = true;
-  
-
-  
-
-
-
-
-
   # Set your time zone.
   time.timeZone = "Asia/Manila";
-
   # Select internationalisation properties.
   i18n.defaultLocale = "en_PH.UTF-8";
-
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "fil_PH";
     LC_IDENTIFICATION = "fil_PH";
@@ -48,162 +34,90 @@
     LC_TELEPHONE = "fil_PH";
     LC_TIME = "fil_PH";
   };
-
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-
-  # Enable the XFCE Desktop Environment.
-  # services.xserver.displayManager.lightdm.enable = true;
-  # services.xserver.desktopManager.xfce.enable = true;
-
   # Configure keymap in X11
   services.xserver.xkb = {
     layout = "us";
     variant = "";
   };
+  # Activate Hyprland
+  programs.hyprland.enable = true;
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
+  environment.variables.EDITOR = "nvim";
 
-  # Enable sound with pipewire.
-  services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
+
+
+
+# Enable greetd display manager
+  services.greetd = {
     enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --remember --cmd Hyprland";
+        user = "greeter";
+      };
+    };
   };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+  
+  # XDG Portal
+  xdg.portal = {
+    enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-hyprland ];
+  };
+  # Enable Audio
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  # Enable bluetooth
+  # hardware.enableAllFirmware = true;
+  hardware.bluetooth.enable = true;
+  # Define a user account. Don't forget to set a password with 'passwd'.
   users.users.meltalizard = {
     isNormalUser = true;
     description = "meltalizard";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-    #  thunderbird
-    ];
+    extraGroups = [ "networkmanager" "wheel" "video" "audio" "input" "storage" ];
+    packages = with pkgs; [];
   };
-  # Install firefox.
-  programs.firefox.enable = true;
-  
-  # Install Hyprland
-  programs.hyprland = {
-	enable = true;
-	xwayland.enable = true;
- };
-  # greetd
- services.greetd = {
-  enable = true;
-  settings = {
-    default_session = {
-    command = "${lib.getExe pkgs.greetd.tuigreet} --time --remember --cmd Hyprland";
-    user = "greeter";
-    };
-  };
-};
-  #kitty
-  #programs.kitty = {
-  #enable = true;
-  #};
-
-  #seatd 
-  services.seatd.enable = true;
-
+ 
+  fonts.packages = with pkgs; [
+    font-awesome
+    nerd-fonts.jetbrains-mono
+    nerd-fonts.symbols-only
+    monaspace
+  ];
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
-
-  # Bluetooth
-    hardware.bluetooth = {
-    enable = true;
-    powerOnBoot = false;
-  };
-  services.blueman.enable = true;
-
-  #Fonts
-  fonts.packages = with pkgs; [
-  nerd-fonts.jetbrains-mono
-  nerd-fonts.fira-code
-  font-awesome
-  ];
-
-  
-
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  waybar
-  wofi
-  hyprpaper
-  brightnessctl
-  nwg-displays
-
-  # For Screenshot
-  grim slurp
-  
-  # For Terminal
-  vim
-  kitty
-  starship
-  wl-clipboard
-  neovim
-  neofetch
-
-  # For Folders
-  xfce.thunar
-  xfce.tumbler
-  ffmpegthumbnailer
-
-
-  obsidian
-  vscode
-
-  #For Development
-  pkgs.git gcc clang
-  gdb lldb
-  cmake
-  flutter dart
-  python315
-  jq
-  
-
-
-  # Network
-  networkmanagerapplet
-  nmap
-
-  # Bluetooth
-  blueman
-
-  # Browsers
-  firefox brave qutebrowser
-
-  arduino-ide
-  vlc
-  imv
-  zip
-  ranger
- 
-  discord
-  htop
-  qbittorrent
-  zathura
-  upower
-
-  # Notifcation
-  libnotify
-  mako
-
-
+    #TUI Based
+    kitty
+    yazi
+    neovim
+    
+    brightnessctl
+    wl-clipboard
+    git
+    obsidian
+    xfce.thunar
+    zip
+    bluez
+    blueman
+    discord
+    # For Ranger
+    xfce.tumbler
+    ffmpegthumbnailer
+    # For Ricing
+    starship
+    waybar
+    wofi
+    pywal
+    neofetch
+    hyprpaper
+    waytrogen
+    
+    #Browsers
+    firefox
+    brave
+    librewolf
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
@@ -213,24 +127,33 @@
   #   enable = true;
   #   enableSSHSupport = true;
   # };
-
   # List services that you want to enable:
-
+  services.dbus.enable = true;
+  security.polkit.enable = true;
+  security.sudo = {
+    enable = true;
+    wheelNeedsPassword = true;
+  };
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
-
+  
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+  };
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
-
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # on your system were taken. It's perfectly fine and recommended to leave
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.11"; # Did you read the comment?
-
 }
