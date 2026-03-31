3396eeaadd0af69df622831439aad8f9bb5c9db6
meltalizard <school.mikotancinco@gmail.com>
2026-03-31 23:02:59 +0800

    Add files via upload

---
+++ hypr/scripts/wallpaper-restore.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+CONFIG="$HOME/.config/waypaper/config.ini"
+WALLPAPER=$(grep "^wallpaper = " "$CONFIG" | cut -d'=' -f2 | xargs)
+WALLPAPER="${WALLPAPER/#\~/$HOME}"
+
+if [ -n "$WALLPAPER" ] && [ -f "$WALLPAPER" ]; then
+    wal -i "$WALLPAPER" -n
+    ln -sf ~/.cache/wal/colors-wofi.css ~/.config/wofi/style.css
+    pkill -USR1 kitty
+    pkill -SIGUSR2 waybar
+fi
